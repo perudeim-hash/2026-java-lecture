058beaf41566e00087cef305f588475a12a98d0a
sejin <perudeim@gmail.com>
2026-05-08 14:26:30 +0900

    2026-05-06 ch14(lambda Method 참조)

---
+++ src/ch14/lamda/methodRef/MethodRefEx01.java
@@ -0,0 +1,29 @@
+package ch14.lamda.methodRef;
+
+import java.util.function.Supplier;
+
+public class MethodRefEx01 {
+    public static void main(String[] args) {
+        // static 메서드 호출
+        Supplier<String> staticMethod01 = () -> Person.greeting();
+        System.out.println("staticMethod01.get() = " + staticMethod01.get());
+        Supplier<String> staticMethod02 =  Person::greeting;
+        System.out.println("staticMethod02.get() = " + staticMethod02.get());
+
+        System.out.println("==================================================");
+        // 특정 객체의 인스턴스 참조
+        Person person = new Person("홍길동");
+        Supplier<String> instanceMethod01 = () -> person.introduce();
+        System.out.println("instanceMethod01.get() = " + instanceMethod01.get());
+        Supplier<String> instanceMethod02 = person::introduce;
+        System.out.println("instanceMethod02.get() = " + instanceMethod02.get());
+
+        System.out.println("==================================================");
+        // 생성자 참조
+        Supplier<Person> newPerson01 = () -> new Person();
+        System.out.println("newPerson01 = " + newPerson01.get());
+        Supplier<Person> newPerson02 = Person::new;
+        System.out.println("newPerson02 = " + newPerson02.get());
+
+    }
+}
---
+++ src/ch14/lamda/methodRef/Person.java
@@ -0,0 +1,28 @@
+package ch14.lamda.methodRef;
+
+public class Person {
+    private String name;
+
+    public Person() {
+        this("Unknown");
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public static String greeting() {
+        return "hello";
+    }
+
+    public static String greetingWithName(String name) {
+        return "hello" + name;
+    }
+    public String introduce() {
+        return "I am " + name;
+    }
+    public String introduceWithNumber(int number) {
+        return "I am " + name + ", my Number is " + number;
+    }
+
+}
